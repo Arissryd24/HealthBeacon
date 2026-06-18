44d4d91a74b33146774c3612ffbc8de97a7e4220
Arissrydi <rysonly@gmail.com>
2026-06-18 22:09:37 +0700

    fix composer install on vercel

--- build.sh
+++ build.sh
@@ -4,11 +4,5 @@
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install --no-dev --optimize-autoloader
 
-# Install npm & build
-npm install
+# Build frontend
 npm run build
-
-# Laravel setup
-cp .env.example .env
-php artisan key:generate
-php artisan config:clear
